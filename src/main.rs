11259063bbc9f810c115995fbd7f72a83767527b
ArcticSpaceFox <Sm03leBr00t@protonmail.com>
2022-02-07 16:48:04 +0100

    :sparkles: take simple argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let message = std::env::args()
+        .nth(1)
+        .expect("Fox does not know what to say. Usage: foxsay <message>");
+    println!("{}", message);
+    println!(" \\");
+    println!("  \\\n");
+    println!("   /\\   /\\   Todd Vargo");
+    println!("  //\\\\_//\\\\     ____");
+    println!("  \\_     _/    /   /");
+    println!("   / * * \\    /^^^]");
+    println!("   \\_\\O/_/    [   ]");
+    println!("    /   \\_    [   /");
+    println!("    \\     \\_  /  /");
+    println!("     [ [ /  \\/ _/");
+    println!("    _[ [ \\  /_/");
 }
